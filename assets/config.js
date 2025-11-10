0a8e69315c5d1cef95c389c2f66cdf650c3ddd09
Georgi Piskov <georgipiskov@gmail.com>
2025-11-10 11:31:38 +0200

    chore(config): set N8N_WEBHOOK_URL to production

--- assets/config.js
+++ assets/config.js
@@ -1,3 +1,2 @@
 // ТУК задайте вашия n8n webhook URL:
-window.N8N_WEBHOOK_URL = ""; // напр. "https://<your-n8n>/webhook/declarations"
-
+window.N8N_WEBHOOK_URL = "https://n8n.simeontsvetanovn8nworkflows.site/webhook/6526ec55-85b5-426f-8625-ccdf7f841153"; // Production URL
